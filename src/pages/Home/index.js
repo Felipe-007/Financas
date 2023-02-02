618a0a90822470254d157e0e04947d894d4627f7
Felipe Lopes <felipe.presidencia@gmail.com>
2023-02-02 14:45:07 -0300

    C15 - Mostrando registros

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,37 +1,57 @@
 /**
  * 
  */
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../../contexts/auth";  //onde são armazenados os dados do usuário
 import { Background, Container, Nome, Saldo, Title, List } from "./styles";
 import Header from "../../components/Header";  //menu de hamburger
 import HistoricoList from "../../components/HistoricoList";
+import firebase from "../../services/firebaseConnection";
+import format from 'date-fns/format';  //faz o controle e formatação das datas
 
 export default function Home() {
 
-  const { user } = useContext(AuthContext); //pega os dados do usuário
+  const [historico, setHistorico] = useState([]);  //lista
+  const [saldo, setSaldo] = useState(0);
 
-  //lista
-  const [historico, setHistorico] = useState([
-    { key: '1', tipo: 'receita', valor: 1200 },
-    { key: '2', tipo: 'despesa', valor: 200 },
-    { key: '3', tipo: 'despesa', valor: 40 },
-    { key: '4', tipo: 'receita', valor: 89.69 },
-    { key: '5', tipo: 'receita', valor: 89.69 },
-    { key: '6', tipo: 'receita', valor: 189.69 },
-    { key: '7', tipo: 'receita', valor: 9.69 },
-    { key: '8', tipo: 'despesa', valor: 69 },
-    { key: '9', tipo: 'receita', valor: 89 },
-    { key: '10', tipo: 'despesa', valor: 89.69 },
-  ]);
+  const { user } = useContext(AuthContext); //pega os dados do usuário
+  const uid = user && user.uid;  //traz o id do usuario
+
+  //será executada quando o aplicativo iniciar
+  useEffect(() => {
+    async function loadList() {
+      await firebase.database().ref('users').child(uid).on('value', (snapshot) => {
+        setSaldo(snapshot.val().saldo);  //atualiza o setSaldo de acordo com o saldo do usuário
+      });
+
+      await firebase.database().ref('historico')
+        .child(uid)
+        .orderByChild('date').equalTo(format(new Date, 'dd/MM/yy'))
+        .limitToLast(10).on('value', (snapshot) => {  //limite de 10 por pagina
+          setHistorico([]);  //zera a lista afim de evitar duplicidade
+
+          snapshot.forEach((childItem) => {
+            let list = {
+              key: childItem.key,
+              tipo: childItem.val().tipo,
+              valor: childItem.val().valor
+            };
+
+            setHistorico(oldArray => [...oldArray, list].reverse());  //pega todos os item que tem na lista e coloca o ultimo digitado, .reverse() = o ultimo sera o primeiro
+          })
+        })
+    }
+
+    loadList();
+  }, []);
 
   return (
     <Background>
       <Header />
 
       <Container>
-        <Nome>{ user && user.nome }</Nome>
-        <Saldo>R$ 20,00</Saldo>
+        <Nome>{user && user.nome}</Nome>
+        <Saldo>R$ {saldo.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')}</Saldo>
       </Container>
 
       <Title>Últimas movimentações</Title>
